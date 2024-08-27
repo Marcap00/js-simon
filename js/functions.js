f5c90859ffef6215fe16bd3eeb076a9a92e5293d
Marcap00 <cappellatomarco@yahoo.com>
2024-08-27 15:51:02 +0200

    add first part

---
+++ js/functions.js
@@ -0,0 +1,38 @@
+// # NUMERO RANDOM
+/**
+ * % Genera un numero random tra un minimo e un massimo (max incluso o no)
+ * @param {number} min Minimo numero che può essere generato (default: 1)
+ * @param {number} max Massimo numero che può essere generato (default: 100)
+ * @param {boolean} isMaxIncluded Se il massimo è incluso o no
+ * @returns {Nan|null|number} Numero casuale o null o NaN
+ */
+function getRandomNumber(min = 1, max = 100, isMaxIncluded = true) {
+    if (isNaN(min) || isNaN(max)) {
+        console.error('min e max devono essere numeri');
+        return NaN;
+    }
+
+    if (min >= max) {
+        console.error('min deve essere inferiore al max');
+        return null;
+    }
+
+    if (isMaxIncluded) max++;
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+
+// # CREATE NODE
+/**
+ * % Funzione che crea un nodo del DOM
+ * @param {string} type Tipo di elemento che vogliamo creare
+ * @param {string} className Quali classi vogliamo dare all'elemento
+ * @param {string|number} content Il tipo di content dell'elemento string o number
+ * @returns {element} l'elemento che abbiamo creato
+ */ 
+function createNode(type, className = '', content = '') {
+    const node = document.createElement(type);
+    node.className = className;
+    node.append(content);
+    return node;
+}
--- js/script.js
+++ js/script.js
@@ -13,7 +13,74 @@ Inseriamo la validazione: se l'utente mette due numeri uguali o inserisce cose d
 
 BONUS 2:
 Generiamo gli input da JS, invece di scriverli nel codice
-
 */
 
-console.log('js ok')
+// Funzione che genera un array di numeri casuali
+function generateRandNumbArr(maxLength) {
+    const randomNumbers = [];
+    while(randomNumbers.length < maxLength) {
+        const randomNumber = getRandomNumber(1, 50);
+        if (!randomNumbers.includes(randomNumber)) randomNumbers.push(randomNumber);
+    }
+    return randomNumbers;
+}
+
+
+/**
+ * % Funzione che crea x nodi
+ * @param {number} max Il massimo di elementi che vogliamo creare
+ * @returns {element} La catena di nodi che è stata creata
+ */
+function createXNode(max) {
+    for (let i = 0; i < max; i++) {
+        const randomNumbers = generateRandNumbArr(maxLength);
+        const randomNumber = randomNumbers[i];
+
+        const randNumbChild = createNode('li', '', randomNumber);
+        
+        
+        RandNumbElement.appendChild(randNumbChild);
+        
+        
+    }
+    return RandNumbElement;
+}
+
+// # fase di preparazione
+// Recuperiamo gli elementi dal DOM
+const form = document.querySelector('form');
+const RandNumbElement = document.getElementById('random-number');
+const counterElement = document.getElementById('counter');
+const inputsNumb = document.getElementById('inputs');
+const button = document.getElementById('button');
+const h2 = document.querySelector('h2');
+const p = document.querySelector('p');
+
+// Preparo le variabili
+// Preparo il numero di elementi max da creare
+const maxLength = 5;
+let counter = 30;
+let randomNumbers = [];
+
+// # Fase di gestione eventi
+// Metto in ascolto sugli eventi il form con l'evento 'click'
+form.addEventListener('click', function(e) {
+    // ! Blocchiamo il comportamento del form
+    e.preventDefault();
+    
+    button.disabled = true;
+
+    createXNode(maxLength);
+    const interval = setInterval(() => {
+        if(counter === 0) {
+            clearInterval(interval, 30000);
+            h2.innerText = 'Inserisci i numeri che ricordi.'
+            p.classList.add('d-none');
+            RandNumbElement.classList.add('d-none');
+            inputsNumb.classList.remove('d-none');
+        }
+        counterElement.innerText = counter--;
+    }, 1000);
+    
+
+})
